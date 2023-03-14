576507da63095245984d226d5dc7a7b3ed20adcc
vania <sk080501@gmail.com>
2023-03-14 08:50:48 +0700

    testin

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -25,53 +25,58 @@ function Navbar() {
             <ul class="navbar-nav col navsatu navawall">
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  About
+                  Wedding
                 </a>
               </li>
               {/* <!-- DROPDOWN-------------------- --> */}
-              <li className="nav-item dropdown navawall">
-                <a
+              {/* <li className="nav-item dropdown navawall"> */}
+                {/* <a
                   className="nav-link dropdown-toggle navmenu dropbtn"
                   id="menuu"
                   role="button"
                   data-bs-toggle="dropdown"
                 >
                   Category
-                </a>
+                </a> */}
 
-                <div className="dropdown-menu mt-1" id="navdd">
+                {/* <div className="dropdown-menu mt-1" id="navdd"> */}
                   {/* <Link to={"/dampak"} style={{ textDecoration: "none" }}> */}
-                  <center>
-                    <a>
+                  {/* <center> */}
+                    {/* <a>
                       <li className="abc">
                         <p className="dropdown-item abc">Party</p>
                       </li>
-                    </a>
+                    </a> */}
                     {/* </Link> */}
                     <hr></hr>
                     {/* <li className="abc"><a className="dropdown-item abc" href="#">Informasi Pencegahan<br></br>Bencana</a></li><hr></hr> */}
                     {/* <Link to={"/pencegahan"} style={{ textDecoration: "none" }}> */}
-                    <li className="abc">
+                    {/* <li className="abc">
                       <p className="dropdown-item abc">Graduation</p>
-                    </li>
+                    </li> */}
                     <hr></hr>
-                    <li className="abc">
+                    {/* <li className="abc">
                       <p className="dropdown-item abc">Engagement</p>
-                    </li>
+                    </li> */}
                     {/* </Link> */}
                     <hr></hr>
                     {/* <li className="abc"><a className="dropdown-item abc" href="#">Artikel Informasi</a></li><hr></hr> */}
                     {/* <Link to={"/forum"} style={{ textDecoration: "none" }}> */}
-                    <li className="abc">
+                    {/* <li className="abc">
                       <p className="dropdown-item abc">Wedding </p>
-                    </li>
-                  </center>
+                    </li> */}
+                  {/* </center> */}
                   {/* </Link> */}
-                </div>
+                {/* </div> */}
+              {/* </li> */}
+              <li class="nav-item navawall">
+                <a class="nav-link " href="#">
+                  Engagement
+                </a>
               </li>
               <li class="nav-item navawall">
                 <a class="nav-link " href="#">
-                  Review
+                  Graduation
                 </a>
               </li>
             </ul>
@@ -79,7 +84,7 @@ function Navbar() {
               <center>
                 <div class="nav-item loredet loginbutton0">
                   <button class="nav-link btn" id="loginbutton" href="#">
-                    Login
+                    About Us
                   </button>
                 </div>
               </center>
@@ -90,7 +95,7 @@ function Navbar() {
                     id="registerbutton"
                     href="#"
                   >
-                    Register &rarr;
+                    Review &rarr;
                   </button>
                 </div>
               </center>
